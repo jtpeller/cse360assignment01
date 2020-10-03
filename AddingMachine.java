0fcd67781bf614c9a6477fa610885bdd5226d77a
jtpeller <70555819+jtpeller@users.noreply.github.com>
2020-10-03 10:54:45 -0700

    Commit #1 - Methods with Javadoc
    Added javadoc

---
+++ AddingMachine.java
@@ -0,0 +1,47 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	  private int total;
+	  
+	  /**
+	   * AddingMachine(): constructor.
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * getTotal(): getter method to retrieve the total.
+	   * @return
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * add(): adds the value to the total
+	   * @param value
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * subtract(): subtracts the value from the total
+	   * @param value
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * toString(): printing method
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * clear(): reset method
+	   */
+	  public void clear() {
+	  }
+	}
